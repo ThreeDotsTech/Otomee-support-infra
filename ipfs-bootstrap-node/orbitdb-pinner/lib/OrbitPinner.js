9e70ddb61a9c0bf8e7ba7b8ebd4ad8c2ae593c17
itsMarcoSolis <marco@threedots.io>
2023-04-02 22:24:56 -0600

    restore lib folder

---
+++ ipfs-bootstrap-node/orbitdb-pinner/lib/OrbitPinner.js
@@ -0,0 +1,98 @@
+import OrbitDB from 'orbit-db'
+import Identities from 'orbit-db-identity-provider'
+import ipfs from './ipfsInstance.js'
+
+let orbitdb
+let ipfsInstance
+
+
+export class Pinner {
+  constructor(db) {
+    this.db = db
+    this.address = db.id
+  }
+
+
+  static async create(address) {
+    console.log('creating db for', address)
+    if (!ipfsInstance) {
+      ipfs.then(newIpfs => {
+        ipfsInstance = newIpfs
+      })
+    }
+    if (!orbitdb) {
+      let identity = await Identities.createIdentity({ type: "ethereum" })
+      orbitdb = await OrbitDB.createInstance(ipfsInstance, { identity })
+    }
+    const db = await Pinner.openDatabase(orbitdb, address)
+    if (!db) return
+    return Promise.resolve(new Pinner(db))
+  }
+
+  static async new(name) {
+    console.log('creating new db with name', name)
+    if (!ipfsInstance) {
+      ipfs.then(newIpfs => {
+        ipfsInstance = newIpfs
+      })
+    }
+    if (!orbitdb) {
+      let identity = await Identities.createIdentity({ type: "ethereum" })
+      orbitdb = await OrbitDB.createInstance(ipfsInstance, { identity })
+    }
+    const db = await Pinner.createDatabase(orbitdb, name)
+    return Promise.resolve(new Pinner(db))
+  }
+
+  drop() {
+    // console.log(this.orbitdb)
+    // this.orbitdb.disconnect()
+  }
+
+  get estimatedSize() {
+    let size = 0
+
+    if (this.db) {
+      // This is very crude
+      size = JSON.stringify(this.db._oplog.values).length
+    }
+
+    return size
+  }
+
+  static async createDatabase(orbitdb, name) {
+    console.log('creating db: ', name)
+    try {
+      const db = await orbitdb.docstore(name, { indexBy: 'hash', accessController: { write: ['*'] } })
+      console.log('Database created')
+      await db.load()
+      console.log(db.id)
+      return db
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  static async openDatabase(orbitdb, address) {
+    console.log('trying to open db', address)
+    //	const name = await orbitdb.docstore('otomee-orderbook-alpha.v0', { indexBy: 'hash', accessController: { write: ['*'] } })
+    //	console.log('created db', name)
+    try {
+      if (!OrbitDB.isValidAddress(address)) {
+        console.log(`Failed to add ${address}. This is not a valid address`)
+        return
+      }
+
+      console.log(`opening database from ${address}`)
+      // Opening timeouts are like so: orbitdb.open(address, { sync: true, timeout: 1000000 })
+      const db = await orbitdb.open(address, { sync: true })
+
+      console.log('Listening for updates to the database...')
+      await db.load()
+
+      return db
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
---
+++ ipfs-bootstrap-node/orbitdb-pinner/lib/pinningList/index.js
@@ -0,0 +1,127 @@
+import OrbitDB from 'orbit-db'
+
+import { Pinner as OrbitPinner } from '../OrbitPinner.js'
+import orbitInstance from './orbitInstance.js'
+
+const pinners = {}
+
+export const getContents =
+  async () => {
+    const db = await orbitInstance()
+    return db.iterator({ limit: -1 })
+      .collect()
+      .map(
+        e => {
+          return e.payload.value
+        }
+      )
+  }
+
+export const getPinners = () => pinners
+
+export const add =
+  async (address) => {
+    const db = await orbitInstance()
+
+    if (!OrbitDB.isValidAddress(address)) {
+      console.log(`Failed to add ${address}. This is not a valid address`)
+      return
+    }
+
+    const addresses = await getContents()
+
+    if (!addresses.includes(address)) {
+      await db.add(address)
+      createPinnerInstance(address)
+    } else {
+      console.warn(`Attempted to add ${address}, but already present in db.`)
+    }
+  }
+
+export const create =
+  async (name) => {
+    const db = await orbitInstance()
+
+    const pinner = await OrbitPinner.new(name)
+
+    await db.add(pinner.address)
+    console.log(`${pinner.address} added to pinner.`)
+    return pinner.address
+  }
+
+const createPinnerInstance =
+  async (address) => {
+    if (!OrbitDB.isValidAddress(address)) {
+      console.log(`Failed to pin ${address}. This is not a valid address`)
+      return
+    }
+
+    console.log(`Pinning orbitdb @ ${address}`)
+    const pinner = await OrbitPinner.create(address)
+    if (!pinner) return
+
+    pinners[address] = pinner
+    console.log(`${address} added to pinnging list.`)
+    return pinners[address]
+  }
+
+const startPinning =
+  async () => {
+    const addresses = await getContents()
+
+    if (addresses.length === 0) {
+      console.log('Pinning list is empty')
+    }
+
+    addresses
+      .map(createPinnerInstance)
+  }
+
+export const remove =
+  async (address) => {
+    if (!OrbitDB.isValidAddress(address)) {
+      console.log(`Failed to unpin ${address}. This is not a valid address`)
+      return
+    }
+
+    if (!pinners[address]) {
+      console.log(`Failed to unpin ${address}. Address not found in pinning list.`)
+      return
+    }
+
+    const db = await orbitInstance()
+    const dbAddresses = await getContents()
+
+    // stop pinning
+    pinners[address].drop()
+    delete pinners[address]
+
+    // Unfortunately, since we can't remove a item from the database without it's hash
+    // So we have to rebuild the data every time we remove an item.
+    await db.drop()
+
+    dbAddresses
+      .filter(addr => (addr !== address))
+      .forEach(
+        address => db.add(address)
+      )
+
+    console.log(`${address} removed.`)
+  }
+
+export const follow =
+  async (address) => {
+    if (!OrbitDB.isValidAddress(address)) {
+      console.log(`Failed to follow ${address}. This is not a valid address`)
+
+      return
+    }
+    console.log('excecuted follow function', address)
+    // await db.drop()
+    await orbitInstance(address)
+    startPinning()
+  }
+
+console.log('Pinning previously added orbitdbs: ')
+startPinning()
+
